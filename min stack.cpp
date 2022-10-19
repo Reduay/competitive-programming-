8624b9dafa4c9a942c53dc39f3ac328a365f6c0d
Reduay <105547293+Reduay@users.noreply.github.com>
2022-10-19 10:03:58 -0700

    Add files via upload

---
+++ min stack.cpp
@@ -0,0 +1,29 @@
+//min stack
+class MinStack {
+public:
+    stack<pair<int, int>>s;
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        if(s.empty()){
+            s.push({val,val});
+        }
+        else{
+            s.push({val, min(val, s.top().second)});
+        }
+    }
+    
+    void pop() {
+        s.pop();    
+    }
+    
+    int top() {
+        return s.top().first;
+    }
+    
+    int getMin() {
+        return s.top().second;
+    }
+};
